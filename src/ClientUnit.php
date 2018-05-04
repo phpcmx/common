9c10f91255708e29b50cb2095bca78ff552410fb
fij_Number <fijdemon@qq.com>
2018-05-04 17:14:04 +0800

    获取客户端ip

---
+++ src/ClientUnit.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caomengxin
+ * Date: 2018/5/4
+ * Time: 下午5:08
+ */
+
+namespace phpcmx\common;
+
+/**
+ * 客户端相关函数
+ * Class ClientUnit
+ *
+ * @package phpcmx\common
+ */
+final class ClientUnit {
+    private function __construct() {}
+
+    /**
+     * 获取客户端ip
+     * @return string
+     */
+    public static function getIp() {
+        if(getenv('HTTP_CLIENT_IP')) {
+            $clientIp = getenv('HTTP_CLIENT_IP');
+        } elseif(getenv('HTTP_X_FORWARDED_FOR')) {
+            $clientIp = getenv('HTTP_X_FORWARDED_FOR');
+        } elseif(getenv('REMOTE_ADDR')) {
+            $clientIp = getenv('REMOTE_ADDR');
+        } else {
+            $clientIp = $HTTP_SERVER_VARS['REMOTE_ADDR'];
+        }
+        return $clientIp;
+    }
+}
